fa119d5d78bb8822af23419101b46321aac3d41c
frankittermann <frank.ittermann@hpi.de>
2017-11-07 17:26:06 +0100

    Fixed cdn publishing took the wrong branch.
    The publish_to_cdn.sh script was taken from wrong branch.

--- scripts/publish_sdk.sh
+++ scripts/publish_sdk.sh
@@ -5,8 +5,8 @@ export PUBLISH_FOLDER="dest"
 export FILENAME="healthcloud_sdk.js"
 export CONTENT_TYPE="application/javascript"
 
-bash <( curl -s -H "Authorization: token ${GITHUB_TOKEN}" -H 'Accept: application/vnd.github.v3.raw' -L https://api.github.com/repos/gesundheitscloud/hc-octopus/contents/remote-scripts/publish_to_cdn.sh?ref=fi/publish_sdk_js)
+bash <( curl -s -H "Authorization: token ${GITHUB_TOKEN}" -H 'Accept: application/vnd.github.v3.raw' -L https://api.github.com/repos/gesundheitscloud/hc-octopus/contents/remote-scripts/publish_to_cdn.sh)
 
 export FILENAME="healthcloud_sdk.js.map"
 export CONTENT_TYPE="application/json"
-bash <( curl -s -H "Authorization: token ${GITHUB_TOKEN}" -H 'Accept: application/vnd.github.v3.raw' -L https://api.github.com/repos/gesundheitscloud/hc-octopus/contents/remote-scripts/publish_to_cdn.sh?ref=fi/publish_sdk_js)
+bash <( curl -s -H "Authorization: token ${GITHUB_TOKEN}" -H 'Accept: application/vnd.github.v3.raw' -L https://api.github.com/repos/gesundheitscloud/hc-octopus/contents/remote-scripts/publish_to_cdn.sh)
