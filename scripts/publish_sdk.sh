32fc500f95e2f40d1fd1ff4d1e73796904036a52
frankittermann <frank.ittermann@hpi.de>
2017-12-19 13:20:29 +0100

    Publish continuously javascript sdk to sandbox
    The javascript sdk for sandbox will continuously publish from the
    master branch. The published filename is 'healthcloud_sdk_latest.js'.
    
    That prevent overwriting the current released javascript sdk
    on sandbox that have the name 'healthcloud_sdk.js'.

--- scripts/publish_sdk.sh
+++ scripts/publish_sdk.sh
@@ -2,11 +2,14 @@
 
 export ENV="$1"
 export PUBLISH_FOLDER="dest"
-export FILENAME="healthcloud_sdk.js"
+export FILE="${PUBLISH_FOLDER}/healthcloud_sdk.js"
+export FILENAME="healthcloud_sdk${FILENAME_SUFFIX}.js"
+
 export CONTENT_TYPE="application/javascript"
 
 bash <( curl -s -H "Authorization: token ${GITHUB_TOKEN}" -H 'Accept: application/vnd.github.v3.raw' -L https://api.github.com/repos/gesundheitscloud/hc-octopus/contents/remote-scripts/publish_to_cdn.sh)
 
-export FILENAME="healthcloud_sdk.js.map"
+export FILE="${PUBLISH_FOLDER}/healthcloud_sdk.js.map"
+export FILENAME="healthcloud_sdk${FILENAME_SUFFIX}.js.map"
 export CONTENT_TYPE="application/json"
 bash <( curl -s -H "Authorization: token ${GITHUB_TOKEN}" -H 'Accept: application/vnd.github.v3.raw' -L https://api.github.com/repos/gesundheitscloud/hc-octopus/contents/remote-scripts/publish_to_cdn.sh)
